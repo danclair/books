2cb0870722a6dc0cec2a74183882fa7945aaa25f
Dan Clair <danclair@me.com>
2021-06-12 15:16:27 -0500

    refine search results

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
-
-const url = `https://www.googleapis.com/books/v1/volumes?q=pizza&key=AIzaSyBgc639eGVx__EC6uT1lVAkKKkghGQrJU4`;
+const searchTerm = 'pizza';
+const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&fields=items(volumeInfo(title,authors,publisher))&maxResults=1&key=AIzaSyBgc639eGVx__EC6uT1lVAkKKkghGQrJU4`;
 
 axios.get(url).then((resp) => {
 	console.log(resp.data.items);
