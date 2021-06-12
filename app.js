ee2c8e7f4ddb3ed020642466b16fff217092cbc6
Dan Clair <danclair@me.com>
2021-06-12 15:48:31 -0500

    clean up url request

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 const searchTerm = 'pizza';
-const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&fields=items(volumeInfo(title,authors,publisher))&maxResults=1&key=AIzaSyBgc639eGVx__EC6uT1lVAkKKkghGQrJU4`;
+const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&fields=items(volumeInfo(title,authors,publisher))&maxResults=1&key=MY_API_KEY`;
+
+let readingList = [];
 
 axios.get(url).then((resp) => {
 	console.log('** Search results: **');
