6f06d443c0c4a452d24c481742c8196823df6319
Dan Clair <danclair@me.com>
2021-06-13 13:23:17 -0500

    print title, author, publisher on separate lines

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const axios = require('axios');
 const prompt = require('prompt-sync')();
 let searchTerm = prompt('Enter a term to search for a book: ');
-const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&fields=items(volumeInfo(title,authors,publisher))&maxResults=1&key=MY_API_KEY`;
+const url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&fields=items(volumeInfo(title,authors,publisher))&maxResults=5&key=AIzaSyBOGGLkQOgeKByLPkFw463lF4THZ8Oo9aI`;
 
 let readingList = [];
 
 axios.get(url).then((resp) => {
 	console.log('** Search results: **');
 	let searchResults = resp.data.items;
-	console.log(searchResults);
+
+	console.log('Title: ' + searchResults[0].volumeInfo.title);
+	console.log('Author: ' + searchResults[0].volumeInfo.authors[0]);
+	console.log('Publisher: ' + searchResults[0].volumeInfo.publisher);
 });
